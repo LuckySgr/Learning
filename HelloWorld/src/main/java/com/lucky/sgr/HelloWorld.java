1cf4f202b26ae75ea25ec5ccd6e1f62138834cd5
谢小龙 <742875110@qq.com>
2022-02-21 11:47:34 +0800

    添加文件

---
+++ HelloWorld/src/main/java/com/lucky/sgr/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.lucky.sgr;
+
+/**
+ * @author：LuckySgr.
+ * @date: 2022/2/21
+ */
+public class HelloWorld {
+}
---
+++ HelloWorld/src/main/java/com/lucky/sgr/MainApp.java
@@ -0,0 +1,11 @@
+package com.lucky.sgr;
+
+/**
+ * @author：LuckySgr.
+ * @date: 2022/2/21
+ */
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
