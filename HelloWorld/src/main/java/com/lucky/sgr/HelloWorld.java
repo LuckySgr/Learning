7159e7fa5d62eadc125dcbfac619d9fd164f4aff
谢小龙 <742875110@qq.com>
2022-02-21 12:03:21 +0800

    提交

--- HelloWorld/src/main/java/com/lucky/sgr/HelloWorld.java
+++ HelloWorld/src/main/java/com/lucky/sgr/HelloWorld.java
@@ -5,4 +5,13 @@ package com.lucky.sgr;
  * @date: 2022/2/21
  */
 public class HelloWorld {
+    private String message;
+
+    public void getMessage() {
+        System.out.println("message:" + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- HelloWorld/src/main/java/com/lucky/sgr/MainApp.java
+++ HelloWorld/src/main/java/com/lucky/sgr/MainApp.java
@@ -1,11 +1,16 @@
 package com.lucky.sgr;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author：LuckySgr.
  * @date: 2022/2/21
  */
 public class MainApp {
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld obj = context.getBean("helloWorld",HelloWorld.class);
+        obj.getMessage();
     }
 }
