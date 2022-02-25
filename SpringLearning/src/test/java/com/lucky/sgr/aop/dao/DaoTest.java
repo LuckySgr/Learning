0071c19ba27e0115d23d3168528add96fa07ff0f
谢小龙 <742875110@qq.com>
2022-02-25 14:18:35 +0800

    添加

---
+++ SpringLearning/src/test/java/com/lucky/sgr/aop/dao/DaoTest.java
@@ -0,0 +1,32 @@
+package com.lucky.sgr.aop.dao;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author：LuckySgr.
+ * @date: 2022/2/25
+ */
+public class DaoTest {
+    @Test
+    public void DaoAllTest() {
+        //获取 ApplicationContext 容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("aop2-beans.xml");
+        //获取代理对象
+        UserDao userDao = context.getBean("userAllDao", UserDao.class);
+        //获取代理对象
+        OrderDao orderDao = context.getBean("orderAllDao", OrderDao.class);
+        //调用 UserDao 中的各个方法
+        userDao.add();
+        userDao.delete();
+        userDao.modify();
+        userDao.get();
+        //调用 OrderDao 中的各个方法
+        orderDao.add();
+        orderDao.adds();
+        orderDao.delete();
+        orderDao.get();
+        orderDao.modify();
+    }
+}
